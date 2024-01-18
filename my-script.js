eb9ded06be24490217bbee176121562e64472a40
andriishmorgan <andrey.shmorgan@gmail.com>
2024-01-18 13:33:52 +0100

    Alaa HM1

---
+++ my-script.js
@@ -0,0 +1,28 @@
+console.log("Day 1 – Javascript console.log Function");
+console.log("The function is declared like this:");
+console.log(`Console.log("What to log")`);
+
+console.log(typeof("Hello world"));
+
+// console log returns value from typeof
+
+typeof ("Hello world");
+// alert(typeof("Hello world"))
+
+console.log(`Hi, guys`);
+
+console.log(1.55);
+
+const userName = "andrey.shmorgan@gmail.com";
+console.log(userName);
+// userName = "Its bad way";
+// console.log(userName); 
+// константу не можемо змінювати, це буде ігнороване
+
+const course = "Javascript";
+let date = 2024;
+console.log("Welcome to the " + course + ", which started on " + date + " to our website");
+
+let num1 = "alla";
+let num2 = 2;
+
