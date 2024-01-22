a3e279bd4569defce9ec5716d5b50e1c5200ac9f
andriishmorgan <andrey.shmorgan@gmail.com>
2024-01-22 21:51:34 +0100

    Commit after 2 lesson

--- my-script.js
+++ my-script.js
@@ -1,28 +1,87 @@
-console.log("Day 1 – Javascript console.log Function");
-console.log("The function is declared like this:");
-console.log(`Console.log("What to log")`);
+// console.log("Day 1 – Javascript console.log Function");
+// console.log("The function is declared like this:");
+// console.log(`Console.log("What to log")`);
 
-console.log(typeof("Hello world"));
+// console.log(typeof("Hello world"));
 
-// console log returns value from typeof
+// // console log returns value from typeof
 
-typeof ("Hello world");
-// alert(typeof("Hello world"))
+// typeof ("Hello world");
+// // alert(typeof("Hello world"))
 
-console.log(`Hi, guys`);
+// console.log(`Hi, guys`);
 
-console.log(1.55);
+// console.log(1.55);
 
-const userName = "andrey.shmorgan@gmail.com";
-console.log(userName);
-// userName = "Its bad way";
-// console.log(userName); 
-// константу не можемо змінювати, це буде ігнороване
+// const userName = "andrey.shmorgan@gmail.com";
+// console.log(userName);
+// // userName = "Its bad way";
+// // console.log(userName);
+// // константу не можемо змінювати, це буде ігнороване
 
-const course = "Javascript";
-let date = 2024;
-console.log("Welcome to the " + course + ", which started on " + date + " to our website");
+// const course = "Javascript";
+// let date = 2024;
+// console.log("Welcome to the " + course + ", which started on " + date + " to our website");
 
-let num1 = "alla";
-let num2 = 2;
+// let num1 = "alla";
+// let num2 = 2;
+
+// let firstName = "Andrew";
+// let lastName = "Shmorhan";
+// let string = "My name is ";
+// let number = 97;
+// let bool = true;
+
+// console.log(firstName, number);
+
+// console.log(`${string}${firstName} ${lastName}`);
+
+// console.log(`${string}${firstName} ${lastName} my ID is`, 100);
+
+// console.log(typeof (firstName));
+
+// console.log(typeof(number));
+
+// console.log(typeof(bool));
+
+// console.log("Lesson 2: String Manipulation");
+// console.log(`You can Concatenate strings together using "+" sign.`);
+// console.log(`For example print("Hello " + "world")`);
+
+// let name = "John"
+// let age = 21;
+// let x = 10 * 5 / 2 + 5;
+
+// console.log(`Hello ${name}, You are ${age} years old`);
+
+// let nameUSer = "Andrew";
+// let ageUser = 23;
+
+// let a = "What is your name?";
+// let b = prompt(a);
+// alert(b);
+
+// // // There are two variables, a and b from input
+// let a = prompt("First Number")
+// let b = prompt("Second Number")
+// // // 🚨 Don't change the code above ☝
+// // //####################################
+// // // Write your code below this line 👇
+
+// let c = a;
+// a = b;
+// b = c;
+
+
+// // // 🚨 Don't change the code below 👇
+// console.log("a: " + a)
+// console.log("b: " + b)
+
+const userName = "Andrew";
+console.log(userName.length);
+
+let name = prompt("What is your name?");
+alert(`Your name has ${name.length} characters`);
+
+
 
