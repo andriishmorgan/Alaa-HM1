6af8365cb64147ae051314e788ba2a6839accb65
andriishmorgan <andrey.shmorgan@gmail.com>
2024-01-24 21:09:02 +0100

    LESSION #4

--- my-script.js
+++ my-script.js
@@ -77,11 +77,53 @@
 // console.log("a: " + a)
 // console.log("b: " + b)
 
-const userName = "Andrew";
-console.log(userName.length);
+// const userName = "Andrew";
+// console.log(userName.length);
 
-let name = prompt("What is your name?");
-alert(`Your name has ${name.length} characters`);
+// let name = prompt("What is your name?");
+// alert(`Your name has ${name.length} characters`);
+
+
+ /* LESSION #4 */
+
+// let userName = "abc";
+// console.log(userName[1]);
+
+// let surName = " Hello";
+
+// console.log(surName.trimEnd());
+// console.log(surName[2]);
+
+// let num = 2;
+// let num2 = 1
+
+// console.log(num - num2 / 6 * 3);
+
+// console.log(3 * 3 + 3 / 3 - 3);
+
+// let a = toString(5)
+
+// console.log(typeof (a));
+
+// let kilomitersN = 20;
+// let milesN = 1.609344;
+
+// let calculate = kilomitersN / milesN; 
+
+// console.log(`${calculate} ml`);
+
+let weight = 75;
+let heightInMeters = 1.75;
+let heightInCentimeters = heightInMeters;
+
+let bodyMassIndex = weight / (heightInCentimeters **2);
+
+console.log(Math.floor(bodyMassIndex));
+
+
+
+
+
 
 
 
