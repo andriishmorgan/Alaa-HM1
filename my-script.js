365154e868c33b1dea13a07603a70667ad1d307b
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2024-03-04 18:40:16 +0100

    commit

--- my-script.js
+++ my-script.js
@@ -650,8 +650,36 @@
 //     console.log(str[count])
 // }
 
-let list1 = ["Phil", "Oz", "Dre"];
-let list2 = [];
+/* ------ HM #13 ------ */
 
-let const userName = "Andrew"
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+/* ------ LESSION #14 ------ */
+// let output = []
+// let count = 1
+
+// function fizzBuzz() {
+//     while (count <= 100) 
+//         if (count % 3 === 0 && count % 5 === 0) {
+//             output.push("FizzBuzz")
+//         } else if (count % 5) {
+//             output.push("Fizz")
+//         } else if (count % 5) {
+//             output.push("Buzz")
+//         } else {
+//             output.push(count)
+//         }
+//     // count++
+//     console.log(output);
+// }
+// fizzBuzz()
 
