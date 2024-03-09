e0f60131438250ce7a9c44131b7b7260869bfe6d
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2024-03-09 09:20:26 +0100

    commit

--- my-script.js
+++ my-script.js
@@ -683,3 +683,37 @@
 // }
 // fizzBuzz()
 
+/* ------ LESSION #14 ------ */
+
+
+// let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+//            'w', 'x', 'y', 'z']
+// let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+// let symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+let symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+const mixArray = letters.concat(numbers, symbols);
+
+function generateRandomString(length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * mixArray.length);
+        result += mixArray[randomIndex];
+    }
+    return result;
+}
+
+const lettersCount = prompt("How many letters?");
+const numbersCount = prompt("How many symbols?");
+const symbolsCount = prompt("How many numbers?");
+
+const randomString = generateRandomString(
+    parseInt(lettersCount) + parseInt(numbersCount) + parseInt(symbolsCount)
+);
+
+console.log(randomString);
